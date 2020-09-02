fa7e2f7ceb17077f64d0486e59f1cfa9fb376689
Jayson Rice <jayson.rice.95@gmail.com>
2020-09-02 15:36:38 -0400

    Max char solution. Added index.html to debug in the browser.

---
+++ exercises/main.js
@@ -0,0 +1,4 @@
+
+import maxChar from "./maxchar/index.js";
+
+maxChar('Check this string please.')
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
 
+    const charMap = {};
+
+    let max = 0;
+    let maxChar = '';
+    // Helper variables to keep track of which character has the most appearances.
+
+    for (const char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+        // Builds a character map object with how many times a character has been repeated.
+        // Char is each character in the string. If the character map already has that character, 
+        // increment it by one. Otherwise set it equal to 1.
+        // Useful start for many string related interview questions.
+    }
+    // Char in the for in loop are the keys of the objects
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char
+        }
+    }
+    return maxChar
+
+}
+// export default maxChar
 module.exports = maxChar;
