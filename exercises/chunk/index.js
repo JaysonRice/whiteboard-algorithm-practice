139f26e1daff617ee0a3012edb79132736dcf8ca
Jayson Rice <jayson.rice.95@gmail.com>
2020-10-22 16:50:39 -0400

    Chunk solution.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+// Solution 1
+function chunk(array, size) {
+    const chunked = [];
+
+    for (const element of array) {
+        // Always grabs the last element (array in this case) in the chunked array
+        const lastElement = chunked[chunked.length - 1];
+
+        // If there are no arrays in chunked OR if the last array in chunked has reached the size limit,
+        // add the element in a new array. All other elements are added to lastElement arrays 
+        // that have not reached their size limit yet  
+        if (!lastElement || lastElement.length === size)
+            chunked.push([element]);
+        else {
+            lastElement.push(element);
+        }
+    }
+    return chunked;
+}
+
 
 module.exports = chunk;
